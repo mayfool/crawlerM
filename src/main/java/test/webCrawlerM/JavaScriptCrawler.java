9878837d1e5b40557f69b1394bdadbaec2f67d6e
unknown <>
2017-07-13 11:24:22 +0800

    动态网站

--- src/main/java/test/webCrawlerM/JavaScriptCrawler.java
+++ src/main/java/test/webCrawlerM/JavaScriptCrawler.java
@@ -7,6 +7,8 @@ import  org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import  java.util.List;
 import  java.util.Random;
   
@@ -16,10 +18,24 @@ public  class  JavaScriptCrawler {
     
     	 System.setProperty("webdriver.chrome.driver","C:\\Users\\Forcast1\\Downloads\\chromedriver_win32\\chromedriver.exe");
     	 WebDriver driver = new ChromeDriver();  
-         driver.get("http://roll.sohu.com/sports/");  
-         Thread.sleep(5000);  
-         WebElement more = driver.findElement(By.cssSelector("#contentA > div.right > div.list14 > ul:nth-child(1) > li:nth-child(4) > a"));  
-         System.out.println(more.getText());
+         driver.get("http://news.163.com/latest/");  
+        // Thread.sleep(5000);  
+         WebElement more;
+         int i=0;
+        // while(true){
+        while(true){
+        	System.out.println(i++);
+        	more=driver.findElement(By.linkText("下一页"));//直接获取的classname
+          //more = driver.findElement(By.xpath("//*[@id=\"instantPanel\"]/div[9]/a[8]"));//*[@id="instantPanel"]/div[9]/a[9]
+         more.click();
+         List<WebElement> lw=new ArrayList<WebElement>();
+         lw=driver.findElements(By.xpath("//div[@id=\"instantPanel\"]/div/ul/li/a[2]"));//从数据库获取xpath
+       //*[@id="artContainer"]/ul[8]/li[1]/a  /ul/li[1]/a //*[@id="artContainer"]/ul[1]/li[2]/a
+         for(WebElement element:lw){
+        	 System.out.println(element.getAttribute("href"));//获取到链接，添加数据库操作
+         }}
+         //System.out.println(more.getText());
+         //}
          /*  more.click();  
          Thread.sleep(5000); // Let the user actually see something!  
          List<WebElement> names = driver.findElements(By.cssSelector("table tbody tr td h4 a"));  
